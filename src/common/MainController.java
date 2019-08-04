c59f51b970360a052f00a2ac3768ece2d21c0dc0
SophYo <sophiya.dimitrova@gmail.com>
2019-08-04 21:47:19 +0300

    constructor written

--- src/common/MainController.java
+++ src/common/MainController.java
@@ -1,10 +1,34 @@
 package common;
 
+import common.interfaces.CommandInterpreter;
 import common.interfaces.InputReader;
 import common.interfaces.OutputWriter;
 
 public class MainController {
     private InputReader reader;
     private OutputWriter writer;
+    private CommandInterpreter commandInterpreter;
+    private boolean isRunning;
 
+    public MainController(InputReader reader, OutputWriter writer,
+                          CommandInterpreter commandInterpreter) {
+        this.reader = reader;
+        this.writer = writer;
+        this.commandInterpreter = commandInterpreter;
+        isRunning = false;
+    }
+
+    public void run() {
+        isRunning = true;
+        StringBuilder aggregator = new StringBuilder();
+
+        while (this.isRunning) {
+            String[] args = this.reader.readLine().split("\\s+");
+            String command = args[0];
+
+
+
+
+        }
+    }
 }
