c638524c5de1fcbc189e8d37f2b48094845fa5da
SophYo <sophiya.dimitrova@gmail.com>
2019-08-04 21:42:35 +0300

    added

---
+++ src/common/MainController.java
@@ -0,0 +1,10 @@
+package common;
+
+import common.interfaces.InputReader;
+import common.interfaces.OutputWriter;
+
+public class MainController {
+    private InputReader reader;
+    private OutputWriter writer;
+
+}
